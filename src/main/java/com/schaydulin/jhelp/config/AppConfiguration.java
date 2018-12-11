0e3c311d919790b99d83f934fbe462be56ef186c
nikolay <blink182u2@gmail.com>
2018-12-11 09:45:29 +0300

    Приложение разворачивается успешно
     - проведено сращивание Spring Boot с Swing
     - добавлен MainFrame.form
     - добавлены новые зависимости в pom.xml

--- src/main/java/com/schaydulin/jhelp/config/AppConfiguration.java
+++ src/main/java/com/schaydulin/jhelp/config/AppConfiguration.java
@@ -1,19 +1,8 @@
 package com.schaydulin.jhelp.config;
 
-import com.schaydulin.jhelp.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class AppConfiguration {
-
-    @Bean
-    @Autowired
-    public Test test(DataSource dataSource) {
-        return new Test(dataSource);
-    }
 
 }
